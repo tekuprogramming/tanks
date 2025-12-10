580948173a9ad3fdf61cd561c4595207b2a21340
tekuprogramming <156584093+tekuprogramming@users.noreply.github.com>
2025-12-10 23:55:30 +0100

    added player tank + its movements

--- main.py
+++ main.py
@@ -5,13 +5,53 @@ HEIGHT = 700
 TITLE = "Tanks"
 
 class Game(arcade.Window):
-    def __init__(self, width, height, title):
+        def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.bg = arcade.load_texture("background.png")
+        # sprites
+        self.green_tank = green_tank.Green_Tank() # sprite tank
+        # fields
+        self.left_pressed = False # left arrow button isn't pressed
+        self.right_pressed = False # right arrow button isn't pressed
+        self.up_pressed = False
+        self.down_pressed = False
 
-    def on_draw(self):
+    def on_draw(self): # responsible for everything drawn in the window
         arcade.draw_texture_rectangle(WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT, self.bg)
-game = Game(width=WIDTH, height=HEIGHT, title=TITLE)
+        self.green_tank.draw()
 
+    def on_key_press(self, key, modifiers): # watches for when the button is pressed
+        if key == arcade.key.LEFT: # if we press the left arrow button
+            self.green_tank.change_angle = 2.5
+            self.left_pressed = True
+            self.right_pressed = False
+        if key == arcade.key.RIGHT:
+            self.green_tank.change_angle = -2.5
+            self.right_pressed = True
+            self.left_pressed = False
+        if key == arcade.key.UP:
+            self.green_tank.change_x = 4
+            self.green_tank.change_y = 4
+            self.up_pressed = True
+            self.down_pressed = False
+        if key == arcade.key.DOWN:
+            self.green_tank.change_x = -3
+            self.green_tank.change_y = -3
+            self.down_pressed = True
+            self.up_pressed = False
 
-arcade.run()
+    def on_key_release(self, key, modifiers): # watches for when the button is released
+        if key == arcade.key.LEFT and not self.right_pressed: # stops the movement when we release the button
+            self.green_tank.change_angle = 0
+        if key == arcade.key.RIGHT and not self.left_pressed:
+            self.green_tank.change_angle = 0
+        if key == arcade.key.UP and not self.down_pressed:
+            self.green_tank.change_x = 0
+            self.green_tank.change_y = 0
+        if key == arcade.key.DOWN and not self.up_pressed:
+            self.green_tank.change_x = 0
+            self.green_tank.change_y = 0
+
+    def update(self, delta_time: float):
+        self.green_tank.update() # allowing tank to move in the window
+
